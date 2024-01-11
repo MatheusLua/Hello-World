90951e7e2c6e1a4db8506135ac552cceb264f768
MatheusLua <156145289+MatheusLua@users.noreply.github.com>
2024-01-11 11:37:17 -0300

    agr sim eu coloquei

---
+++ Lua1.lua
@@ -0,0 +1,8 @@
+os.execute("chcp 65001")
+
+local name = "MatheusLua"
+-- se for usar esse código, mude para seu nome e outros detalhes.
+local game = "Pizza Tower"
+-- pode mudar isso tbm (se o seu jogo de console favorito não for esse)
+print("-oi, " .. name .. " qual é seu jogo favorito?")
+print("é " .. game)
